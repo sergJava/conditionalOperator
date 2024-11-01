be80ed3775d63bcf842cbd539354992befc8d2a3
sergf <sergfibih@ya.ru>
2024-11-01 14:35:22 +1000

    сделал 1ю и 2ю задачи

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 11;
+        System.out.println("Task 1");
+        int age = 17;
         if (age>=18){
             System.out.println("если возраст человека равен " +age +", то он совершеннолетний");
         } else {
             System.out.println("если возраст человека равен " +age +", то он не достиг " +
                     "совершеннолетия, надо подождать");
         }
+        System.out.println("_______________________________________________________________________");
+
+        System.out.println("Task 2");
+        int temperature = 4;
+        System.out.print("На улице " +temperature +" градусов,");
+        if(temperature >=5){
+            System.out.print(" можно идти без шапки.");
+        } else {
+            System.out.print(" нужно надеть шапку.");
+        }
+        System.out.println("_______________________________________________________________________");
+
+        System.out.println("Task 3");
+
     }
 }
